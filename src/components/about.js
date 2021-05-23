14e572364c59c5e0b5b7767bdb92aff12775f3e8
Abhishek Singh <abhishek.dce04@gmail.com>
2021-05-23 16:12:03 +1000

    Safe Checkpoint for experimental changes

--- src/components/about.js
+++ src/components/about.js
@@ -5,61 +5,71 @@ import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 import { PieChart } from 'react-minimal-pie-chart';
 import Sidebar from "./sidebar.js";
 import "../style/analysis.css";
+import StudentSurvey from "./Survey";
 const calculateCorrelation = require("calculate-correlation");
 const { Component } = require("react");
 var totalSurvey  = 0;
+
+
+var pearSonCorr = {
+    q1_q8 : 0,
+    q2_q8 : 0,
+    q3_q8 : 0,
+    q4_q8 : 0,
+    q5_q8 : 0,
+    q6_q8 : 0,
+    q7_q8 : 0,
+
+}
+
+function Calculate_pearSonCorr(arrData)
+{
+    // debugger;
+    console.log("arrData");
+    // console.log(arrData.q1_ans_Num);
+    const x = arrData.q1_ans_Num;
+    const y = arrData.q8_ans_Num;
+    // console.log("x");
+    // console.log(x);
+
+    // console.log("y");
+    // console.log(y);
+    // pearSonCorr.q1_q8 =calculateCorrelation(arrData.q1_ans_Num, arrData.q8_ans_Num);
+    // pearSonCorr.q2_q8 =calculateCorrelation(arrData.q2_ans_Num, arrData.q8_ans_Num);
+    // pearSonCorr.q3_q8 =calculateCorrelation(arrData.q3_ans_Num, arrData.q8_ans_Num);
+    // pearSonCorr.q4_q8 =calculateCorrelation(arrData.q4_ans_Num, arrData.q8_ans_Num);
+    // pearSonCorr.q5_q8 =calculateCorrelation(arrData.q5_ans_Num, arrData.q8_ans_Num);
+    // pearSonCorr.q6_q8 =calculateCorrelation(arrData.q6_ans_Num, arrData.q8_ans_Num);
+    // pearSonCorr.q7_q8 =calculateCorrelation(arrData.q7_ans_Num, arrData.q8_ans_Num);
+
+}
+
+
+
+
+
 var StudentResponses =
-            {
-              q1_ans1_Num : 0,
-              q1_ans2_Num : 0,
-              q1_ans3_Num : 0,
-            
-              q2_ans1_Num : 0,
-              q2_ans2_Num : 0,
-            
-              q3_ans1_Num : 0,
-              q3_ans2_Num : 0,
-            
-              q4_ans1_Num : 0,
-              q4_ans2_Num : 0,
-            
-              q5_ans1_Num : 0,
-              q5_ans2_Num : 0,
-            
-              q6_ans1_Num : 0,
-              q6_ans2_Num : 0,
-            
-              q7_ans1_Num : 0,
-              q7_ans2_Num : 0,
+{
+    q1_ans_Num : [],
+    q2_ans_Num : [],
+    q3_ans_Num : [], 
+    q4_ans_Num : [],
+    q5_ans_Num : [],
+    q6_ans_Num : [],
+    q7_ans_Num : [],
+    q8_ans_Num : [],
 
-              q8_ans1_Num : 0,
-              q8_ans2_Num : 0,
 
-              q1_ans1_Cat : 0,
-              q1_ans2_Cat : 0,
-              q1_ans3_Cat : 0,
-            
-              q2_ans1_Cat : 0,
-              q2_ans2_Cat : 0,
-            
-              q3_ans1_Cat : 0,
-              q3_ans2_Cat : 0,
-            
-              q4_ans1_Cat : 0,
-              q4_ans2_Cat : 0,
-            
-              q5_ans1_Cat : 0,
-              q5_ans2_Cat : 0,
-            
-              q6_ans1_Cat : 0,
-              q6_ans2_Cat : 0,
-            
-              q7_ans1_Cat : 0,
-              q7_ans2_Cat : 0,
-                          
-              q8_ans1_Cat : 0,
-              q8_ans2_Cat : 0
-            };
+    q1_ans_Cat : [],
+    q2_ans_Cat : [],
+    q3_ans_Cat : [],            
+    q4_ans_Cat : [],
+    q5_ans_Cat : [],
+    q6_ans_Cat : [],
+    q7_ans_Cat : [],
+    q8_ans_Cat : []
+};
+
 
 //Import the Data from Real Time Database:
 function getDataFromDB() {
@@ -70,9 +80,69 @@ function getDataFromDB() {
     var childData = childSnapshot.val();
     
     //Display the value in console
-    console.log(childData.answers);
+    // console.log(childData.answers);
 
+    //Save the value in the Data Structure:
+    //Questions1.
+    // debugger;
+    StudentResponses.q1_ans_Cat.push(childData.answers.ans1);
+    if      (childData.answers.ans1 == "Not specific") {  StudentResponses.q1_ans_Num.push(0);}
+            
+    else if (childData.answers.ans2 == "Night time")   {  StudentResponses.q1_ans_Num.push(1);}
+            
+    else                                                { StudentResponses.q1_ans_Num.push(2);}
+        
+    //Question2.
+    StudentResponses.q2_ans_Cat.push(childData.answers.ans2);
+    if(childData.answers.ans2 == "Yes")
+            StudentResponses.q2_ans_Num.push(0);
+    else if (childData.answers.ans2 == "No")
+            StudentResponses.q2_ans_Num.push(1);
+
+    //Question3.
+    StudentResponses.q3_ans_Cat.push(childData.answers.ans3);
+    if(childData.answers.ans3 == "Often")
+            StudentResponses.q3_ans_Num.push(0);
+    else if (childData.answers.ans3 == "Seldom")
+            StudentResponses.q3_ans_Num.push(1);
+
+
+     //Question4.
+     StudentResponses.q4_ans_Cat.push(childData.answers.ans4);
+     if(childData.answers.ans4 == "Often")
+             StudentResponses.q4_ans_Num.push(0);
+     else if (childData.answers.ans4 == "Seldom")
+             StudentResponses.q4_ans_Num.push(1);
+
+      //Question5.
+      StudentResponses.q5_ans_Cat.push(childData.answers.ans5);
+      if(childData.answers.ans5 == "Yes")
+              StudentResponses.q5_ans_Num.push(0);
+      else if (childData.answers.ans5 == "No")
+              StudentResponses.q5_ans_Num.push(1);
+
+       //Question6.
+       StudentResponses.q6_ans_Cat.push(childData.answers.ans6);
+       if(childData.answers.ans6 == "Under Graduate")
+               StudentResponses.q6_ans_Num.push(0);
+       else if (childData.answers.ans6 == "Post Graduate    ")
+               StudentResponses.q6_ans_Num.push(1);
+
+        //Question7.
+        StudentResponses.q7_ans_Cat.push(childData.answers.ans7);
+        if(childData.answers.ans7 == "Qualitative")
+                StudentResponses.q7_ans_Num.push(0);
+        else if (childData.answers.ans7 == "Quantitative")
+                StudentResponses.q7_ans_Num.push(1);
+
+        //Question8.
+        StudentResponses.q8_ans_Cat.push(childData.answers.ans8);
+        if(childData.answers.ans8 == "Synchronous")
+                StudentResponses.q8_ans_Num.push(0);
+        else if (childData.answers.ans8 == "Asynchronous")
+                StudentResponses.q8_ans_Num.push(1);
       });
+      
     });
   }
   
@@ -81,7 +151,9 @@ class About extends Component
     state = {
         show_T_Test : false,
         showCorr    : false,
-        showNeurNet : false
+        showNeurNet : false,
+        StudentResponses : {},
+        pearSonCorr : {}
     }
 
     handleClick = (componentSelected) =>{
@@ -94,6 +166,7 @@ class About extends Component
 
     render()
     {
+
         const mystyle1 = {
             color: "white",
             position: "absolute",
@@ -104,6 +177,11 @@ class About extends Component
           };
       
         getDataFromDB();
+        console.log("Total Survey: ", totalSurvey);
+        console.log(StudentResponses);
+        // console.log( pearSonCorr.q1_q8);
+        Calculate_pearSonCorr(StudentResponses);
+        // debugger;
         const x = [2, 5, 4, 1];
         const y = [3, 3, 6, 7];
         const correlation = calculateCorrelation(x, y);
